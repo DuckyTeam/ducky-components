59832187fe5aadffecc36018102e7fc2e5021271
Christian Alfoni <christianalfoni@gmail.com>
2015-11-21 15:35:12 +0100

    Removed Ducky component

--- preview/App.js
+++ preview/App.js
@@ -3,8 +3,6 @@ import styles from './App.css';
 import Preview from './Preview';
 import 'font-awesome/css/font-awesome.css';
 
-import Ducky from '../mobile/Ducky';
-
 export default class App extends React.Component {
   render() {
     return (
@@ -12,7 +10,7 @@ export default class App extends React.Component {
         <h1>/common</h1>
         <h1>/mobile</h1>
         <Preview title="/Ducky" width={200}>
-          <Ducky title="Hello world!" className={styles.pointsColor}/>
+          test
         </Preview>
         <h1>/pad</h1>
         <h1>/desktop</h1>
