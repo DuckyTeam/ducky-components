637b2a4f52c4a3027fc7eb72f97f294d03fe5aa9
Johannes Andersen <johanes1@online.no>
2015-11-21 17:03:28 +0100

    Working IconImage

--- preview/App.js
+++ preview/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import styles from './App.css';
 import Preview from './Preview';
 import 'font-awesome/css/font-awesome.css';
+import IconImage from "./../common/IconImage/index.js";
 
 export default class App extends React.Component {
   render() {
     return (
       <div className={styles.wrapper}>
         <h1>/common</h1>
+          <Preview title="/IconImage" width={200}>
+              <IconImage/>
+          </Preview>
         <h1>/mobile</h1>
         <Preview title="/Ducky" width={200}>
-          test
+            <Ducky title="Hello world!" className={styles.pointsColor}/>
         </Preview>
         <h1>/pad</h1>
         <h1>/desktop</h1>
