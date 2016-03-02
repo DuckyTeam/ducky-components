d56f21d0d7f31d5d77f1d5454f156faf658a125a
Emil Lysholm <emil.lysholm@gmail.com>
2016-03-02 11:50:39 +0100

    replace body for now, workaround for problems with html plugin

--- preview/main.js
+++ preview/main.js
@@ -2,4 +2,4 @@ import App from './App.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.body);
