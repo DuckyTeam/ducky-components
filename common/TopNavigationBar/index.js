750a31f4a58eba866b41ecc363158edc51656ac5
Johannes Andersen <johanes1@online.no>
2016-03-02 15:56:16 +0100

    WIP

--- common/TopNavigationBar/index.js
+++ common/TopNavigationBar/index.js
@@ -1,24 +1,32 @@
 import React from "react";
 import styles from "./TopNavigationBar.css";
+import icons from './../duckyIcons.css';
+import Typography from "./../Typography";
+import Avatar from "./../Avatar";
+
+function getNotificationCounter(number) {
+    if (number === 0) {
+        return (<i className={`zmdi zmdi-notifications-none ${styles.rightIcon}`}/>);
+    }else if (number < 10) {
+        return (<i className={`zmdi zmdi-Ingress_Normal ${styles.notificationNumber} ${styles.rightIcon}`}>{number}</i>);
+    }else{
+        return (<i className={`zmdi zmdi-Ingress_Normal ${styles.notificationNumber} ${styles.rightIcon}`}>{"9+"}</i>);
+    }
+}
 
 class TopNavigationBar extends React.Component {
+
     render() {
         return (
             <div className={styles.container}>
-                <i className={`zmdi zmdi-menu ${styles.menuIcon}`} />
-                <img
-                    className={styles.logo}
-                    src="https://www.ducky.no/images/ducky-logo-no-padding.png"
-                />
-                <p className={`zmdi zmdi-Ingress_Normal ${styles.split}`}>
-                    {"|"}
-                </p>
-                <p className={`zmdi zmdi-Ingress_Normal ${styles.title}`}>
-                    {"Tittel"}
-                </p>
-                <i className={`zmdi zmdi-account-circle ${styles.rightIcon}`} />
-                <i className={`zmdi zmdi-notifications-none ${styles.rightIcon}`} />
-                <i className={`zmdi zmdi-search ${styles.rightIcon}`} />
+                <a onClick={this.props.onClickMenu}><i className={`zmdi zmdi-menu ${icons.duckyconMenu}`}/></a>
+                <a onClick={this.props.onClickDucky}><img className={styles.logo} src="https://www.ducky.no/images/ducky-logo-no-padding.png"/></a>
+                <Typography className={styles.split} type={"ingressNormal"}>{"|"}</Typography>
+                <Typography className={styles.title} type={"ingressNormal"}>{this.props.title}</Typography>
+                <a onClick={this.props.onClickProfile}><i className={`zmdi zmdi-account-circle ${styles.rightIcon}`}/></a>
+                <a onClick={this.props.onClickProfile}><Avatar className={styles.rightIcon} size={"small"}/></a>
+                <a onClick={this.props.onClickNotification}>{getNotificationCounter(this.props.notifications)}</a>
+                <a onClick={this.props.onClickSearch}><i className={`zmdi zmdi-search ${styles.rightIcon}`}/></a>
             </div>
         );
     }
