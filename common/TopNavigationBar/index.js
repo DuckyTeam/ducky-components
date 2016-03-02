fa0d11ceb8bed66b43325f93e2598a49d96314f1
Johannes Andersen <johanes1@online.no>
2016-03-02 16:45:38 +0100

    Removed custom fontRemoved custom fontss

--- common/TopNavigationBar/index.js
+++ common/TopNavigationBar/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import styles from "./TopNavigationBar.css";
-import icons from './../duckyIcons.css';
 import Typography from "./../Typography";
 import Avatar from "./../Avatar";
 
 function getNotificationCounter(number) {
     if (number === 0) {
-        return (<i className={`zmdi zmdi-notifications-none ${styles.rightIcon}`}/>);
+        return (<i className={`zmdi zmdi-notifications-none ${""}`}/>);
     }else if (number < 10) {
         return (<i className={`zmdi zmdi-Ingress_Normal ${styles.notificationNumber} ${styles.rightIcon}`}>{number}</i>);
     }else{
@@ -19,7 +18,7 @@ class TopNavigationBar extends React.Component {
     render() {
         return (
             <div className={styles.container}>
-                <a onClick={this.props.onClickMenu}><i className={`zmdi zmdi-menu ${icons.duckyconMenu}`}/></a>
+                <a onClick={this.props.onClickMenu}><i className={`zmdi zmdi-menu ${""}`}/></a>
                 <a onClick={this.props.onClickDucky}><img className={styles.logo} src="https://www.ducky.no/images/ducky-logo-no-padding.png"/></a>
                 <Typography className={styles.split} type={"ingressNormal"}>{"|"}</Typography>
                 <Typography className={styles.title} type={"ingressNormal"}>{this.props.title}</Typography>
