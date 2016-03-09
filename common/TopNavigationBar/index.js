8cb68af5f91aa8a16065546b25f6b1d3ec5645a6
Johannes Andersen <johanes1@online.no>
2016-03-09 10:12:56 +0100

    Working topnavbar

--- common/TopNavigationBar/index.js
+++ common/TopNavigationBar/index.js
@@ -1,28 +1,72 @@
+import Avatar from "./../Avatar";
 import React from "react";
+import Typography from "./../Typography";
 import styles from "./TopNavigationBar.css";
 
+const MAX_NOTIFICATION_NUMBER = 10;
+
+function getNotificationCounter(number) {
+    if (number === 0) {
+        return (<i className={`zmdi zmdi-notifications-none ${""}`} />);
+    } else if (number < MAX_NOTIFICATION_NUMBER) {
+        return (<i className={`zmdi zmdi-Ingress_Normal` +
+        `${styles.notificationNumber} ${styles.rightIcon}`}
+                >{number}</i>);
+    }
+    return (<i className={`zmdi zmdi-Ingress_Normal` +
+    ` ${styles.notificationNumber} ${styles.rightIcon}`}
+            >{"9+"}</i>);
+}
+
 class TopNavigationBar extends React.Component {
+
     render() {
         return (
             <div className={styles.container}>
-                <i className={`zmdi zmdi-menu ${styles.menuIcon}`} />
-                <img
-                    className={styles.logo}
-                    src="https://www.ducky.no/images/ducky-logo-no-padding.png"
-                />
-                <p className={`zmdi zmdi-Ingress_Normal ${styles.split}`}>
+                <a onClick={this.props.onClickMenu}>
+                    <i className={`${styles.menuIcon} duckycon-duckylogo`} />
+                </a>
+                <a onClick={this.props.onClickDucky}>
+                    <i className={"duckyconDuckylogo"} />
+                </a>
+                <Typography className={styles.split}
+                    type={"ingressNormal"}
+                >
                     {"|"}
-                </p>
-                <p className={`zmdi zmdi-Ingress_Normal ${styles.title}`}>
-                    {"Tittel"}
-                </p>
-                <i className={`zmdi zmdi-account-circle ${styles.rightIcon}`} />
-                <i className={`zmdi zmdi-notifications-none ${styles.rightIcon}`} />
-                <i className={`zmdi zmdi-search ${styles.rightIcon}`} />
+                </Typography>
+                <Typography className={styles.title}
+                    type={"ingressNormal"}
+                >
+                    {this.props.title}
+                </Typography>
+                <a onClick={this.props.onClickProfile}>
+                    <i className={`zmdi zmdi-account-circle ${styles.rightIcon}`} />
+                </a>
+                <a onClick={this.props.onClickProfile}>
+                    <Avatar className={styles.rightIcon}
+                        size={"small"}
+                    />
+                </a>
+                <a onClick={this.props.onClickNotification}>
+                    {getNotificationCounter(this.props.notifications)}
+                </a>
+                <a onClick={this.props.onClickSearch}>
+                    <i className={`zmdi zmdi-search ${styles.rightIcon}`} />
+                </a>
             </div>
         );
     }
 }
+
+TopNavigationBar.propTypes = {
+    notifications: React.PropTypes.number,
+    onClickDucky: React.PropTypes.func,
+    onClickMenu: React.PropTypes.func,
+    onClickNotification: React.PropTypes.func,
+    onClickProfile: React.PropTypes.func,
+    onClickSearch: React.PropTypes.func,
+    title: React.PropTypes.string
+};
 
 TopNavigationBar.displayName = "TopNavBar";
 export default TopNavigationBar;
