1218743372ed72afa041f72c13c93331def9dc4f
Emil Lysholm <emil.lysholm@gmail.com>
2016-04-06 13:00:14 +0200

    Lint ScrollContainer

--- common/ScrollContainer/index.js
+++ common/ScrollContainer/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import {PropTypes} from 'react';
 import styles from './styles.css';
 
 function ScrollContainer(props) {
     return (
-        <div className={styles.wrapper} style={{maxHeight: props.size}}>
+        <div
+            className={styles.wrapper}
+            style={{maxHeight: props.size}}
+        >
             {props.children}
         </div>
     );
 }
 
 ScrollContainer.propTypes = {
-    children: React.PropTypes.any,
-    size: React.PropTypes.number
+    children: PropTypes.node,
+    size: PropTypes.number
 };
 
 export default ScrollContainer;
