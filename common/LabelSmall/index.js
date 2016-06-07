f8b2b0dc92f8c6cfef9c4d6f2f30b94c846de1bf
sara3988 <sara3988@gmail.com>
2016-06-07 11:41:59 +0200

    Commit for COLab01-957

--- common/LabelSmall/index.js
+++ common/LabelSmall/index.js
@@ -11,7 +11,8 @@ function LabelSmall(props) {
             <Icon
                 className={classNames(styles.pad, {
                     [styles.pointsIcon]: props.icon === 'icon-brightness_high',
-                    [styles.co2Icon]: props.icon === 'icon-leaf'
+                    [styles.co2Icon]: props.icon === 'icon-leaf',
+                    [props.className]: props.className
                 })}
                 icon={props.icon}
                 onClick={props.onClick}
---
+++ common/composites/ChallengeCardLabel1/index.js
@@ -0,0 +1,85 @@
+import ProgressBar from '../../ProgressBar';
+import Typography from '../../Typography';
+import LabelSmall from '../../LabelSmall';
+import classNames from 'classNames';
+import Spacer from '../../Spacer';
+import React from 'react';
+import styles from './styles.css';
+const PropTypes = React.PropTypes;
+
+function ChallengeCardLabel1(props) {
+    let pColor = '';
+    let iconName = '';
+    let typoText = '';
+    let opt = '';
+    let percentValue = props.percentValue > 0 ? props.percentValue : 0;
+
+    if (props.type === 'points') {
+        pColor = '#8BC34A';
+        iconName = 'icon-brightness_high';
+        typoText = 'Poeng';
+    } else if (props.type === 'co2') {
+        pColor = '#00AB97';
+        iconName = 'icon-leaf';
+        typoText = 'kgCO2e';
+    } else if (props.type === 'activities') {
+        pColor = '#0099A9';
+        iconName = 'icon-check_circle';
+        typoText = 'Aktiviteter';
+    }
+
+    if (props.goal) {
+        opt = (<Typography className={styles.pad}
+            type={'caption2Normal'}
+               >
+            {percentValue}{'%'}
+        </Typography>);
+    } else {
+        opt = (<LabelSmall
+            className={classNames(styles.labelSmall, {
+                [styles.leader]: props.user === 'leader',
+                [styles.user]: props.user !== 'leader'
+            })}
+            content={props.percentValue > 0 ? props.percentValue : '-'}
+            icon={props.user === 'leader' ? 'icon-crown' : 'icon-hashtag'}
+            type={"caption2Strong"}
+               />
+        );
+    }
+    return (
+        <span>
+            <ProgressBar
+                color={pColor}
+                percent={props.progPercent}
+            />
+            <Spacer size={'standard'} />
+            <span className={styles.wrapper}>
+                <LabelSmall
+                    className={classNames(styles.general,
+                            styles[props.type]
+                            )}
+                    content={props.points > 0 ? props.points : '-'}
+                    icon={iconName}
+                    type={"caption2Strong"}
+                />
+                <Typography className={styles.typoText}
+                    type={'caption2Normal'}
+                >
+                    {typoText}
+                </Typography>
+                    {opt}
+            </span>
+        </span>
+        );
+}
+
+
+ChallengeCardLabel1.propTypes = {
+    goal: PropTypes.bool,
+    percentValue: PropTypes.number,
+    points: PropTypes.number,
+    progPercent: PropTypes.number,
+    type: PropTypes.string,
+    user: PropTypes.string
+};
+export default ChallengeCardLabel1;
