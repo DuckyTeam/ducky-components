db95c3799705a31fe28365c834dfa78eaabbe765
sara3988 <sara3988@gmail.com>
2016-06-08 16:05:17 +0200

    Commit for PoMA03-PopoverMenuAnchor3-769

---
+++ common/PopoverMenuAnchor3/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {PropTypes} from 'react';
+import classNames from 'classnames';
+import styles from './styles.css';
+import Icon from '../Icon';
+import Typography from '../Typography';
+
+function PopoverMenuAnchor3(props) {
+    return (
+        <div
+            className={classNames(styles.wrapper, {[props.className]: props.className})}
+            onClick={props.onClick}
+        >
+
+            <Typography
+                className={styles.text}
+                type="ingressStrong"
+            >
+                {props.children}
+            </Typography>
+            <Icon
+                icon="icon-arrow_drop_down"
+                size="standard"
+            />
+        </div>
+    );
+}
+
+PopoverMenuAnchor3.propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string,
+    onClick: PropTypes.func
+};
+
+export default PopoverMenuAnchor3;
