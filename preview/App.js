4eda0202fd2ca04f258858f95aa150f62fbaaae6
sara3988 <sara3988@gmail.com>
2016-06-09 13:35:37 +0200

    Commit for COHead01-ChalOverHeader1-970

--- preview/App.js
+++ preview/App.js
@@ -15,6 +15,7 @@ import ButtonIcon from '../common/ButtonIcon';
 import ButtonRaised from '../common/ButtonRaised';
 import ButtonCounter from '../common/ButtonCounter';
 import CardLabel1 from '../common/composites/ChallengeCardLabel1';
+import ChalOverHead1 from '../common/ChallengeOverviewHeader1';
 import CheckBox from '../common/CheckBox';
 import CardLabel2 from '../common/ChallengeCardLabel2';
 import ConfirmationModal from '../common/ConfirmationModal';
@@ -1117,6 +1118,12 @@ export default class App extends React.Component {
                         labelContent={"2,355"}
                         text={"Fullført 23. Apr"}
                     />
+                </Preview>
+
+                <Preview title="ChallengeCardLabel2">
+                    <div style={{backgroundColor: 'lightgrey'}}>
+                        <ChalOverHead1 />
+                    </div>
                 </Preview>
 
                 <Preview title={"IconSVG"}>
