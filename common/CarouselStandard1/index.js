e06b852defc765af9665163eaf3baea5030f0ec2
sara3988 <sara3988@gmail.com>
2016-07-05 12:07:58 +0200

    Commit for CarouselStandard1

---
+++ common/CarouselStandard1/index.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import CarouselNavigator from '../CarouselNavigator1';
+import IconAvaWrapper from '../composites/IconAvaWrapper';
+// import classNames from 'classnames';
+import styles from './styles.css';
+// let slideIndex = 1;
+
+class CarouselStandard1 extends React.Component {
+
+/*    showSlides(slideNumber) {
+        let counter = 0;
+        const slides = document.getElementsByClassName("mySlides");
+        const dots = document.getElementById("carouselNavigator");
+
+        if (slideNumber > this.props.slideCount) {
+            slideIndex = 1;
+        }
+
+        if (slideNumber > 1) {
+            slideIndex = this.props.slideCount;
+        }
+
+        for (counter = 0; counter < this.props.slideCount; counter += 1) {
+            slides[counter].style.display = "none";
+        }
+
+        for (counter = 0; counter < dots.length; counter += 1) {
+            dots[counter].className = dots[counter].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+    }
+
+    handleplusSlides(slideNumber) {
+        this.showSlides(slideIndex += slideNumber);
+    }
+
+    currentSlide(slideNumber) {
+        this.showSlides(slideIndex = slideNumber);
+    }
+*/
+    render() {
+        return (
+            <div className={styles.outerWrapper}>
+                <div className={styles.innerWrapper}>
+                    <IconAvaWrapper
+                        className={this.props.leftactive ? styles.iconActive : styles.iconInactive}
+                        icon={'icon-keyboard_arrow_left'}
+                        // onClick={this.handleplusSlides(-1)}
+                        onClick={this.props.handleLeftClick}
+                    />
+                    <div className={"mySlides"}>
+                        {this.props.children}
+                    </div>
+                    <IconAvaWrapper
+                        className={this.props.rightactive ? styles.iconActive : styles.iconInactive}
+                        icon={'icon-keyboard_arrow_right'}
+                        // onClick={this.handleplusSlides(1)}
+                        onClick={this.props.handleRightClick}
+                    />
+                </div>
+                <div className={styles.carouselNavigator}
+                    id={"carouselNavigator"}
+                >
+                    <CarouselNavigator
+                        slideCount={this.props.slideCount}
+                        theme={'dark'}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+CarouselStandard1.propTypes = {
+    children: React.PropTypes.string,
+    handleLeftClick: React.PropTypes.func,
+    handleRightClick: React.PropTypes.func,
+    leftactive: React.PropTypes.bool,
+    rightactive: React.PropTypes.bool,
+    slideCount: React.PropTypes.number,
+    theme: React.PropTypes.string
+};
+
+export default CarouselStandard1;
