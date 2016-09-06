5a8dda168a0ac49d5d0373548a5db6a051ae8501
sara3988 <sara3988@gmail.com>
2016-09-06 11:16:20 +0200

    Commit for PoMA04-PopovermenuAnchor#1575
    Github issue: 1575

---
+++ src/PopoverMenuAnchor4/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {PropTypes} from 'react';
+import classNames from 'classnames';
+import styles from './styles.css';
+import Icon from '../Icon';
+import Typography from '../Typography';
+
+function PopoverMenuAnchor4(props) {
+    return (
+        <div
+            className={classNames(styles.wrapper, {[props.className]: props.className})}
+            onClick={props.onClick}
+        >
+            <Typography
+                className={styles.text}
+                type={"bodyTextNormal"}
+            >
+                {props.children}
+            </Typography>
+            <Icon
+                className={styles.icon}
+                icon={"icon-arrow_drop_down"}
+                size={"standard"}
+            />
+        </div>
+    );
+}
+
+PopoverMenuAnchor4.propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string,
+    onClick: PropTypes.func
+};
+
+export default PopoverMenuAnchor4;
