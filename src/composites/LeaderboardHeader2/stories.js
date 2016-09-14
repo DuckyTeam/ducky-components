b846de30dd40c3697285491d437b464177fb8b07
sara3988 <sara3988@gmail.com>
2016-09-14 16:27:59 +0200

    commit for LBHead2-LeaderboardHeader2#1638
    Github issues: 1638 and 1637(log button inactive state)

---
+++ src/composites/LeaderboardHeader2/stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf, action, linkTo } from '@kadira/storybook';
+import LeaderboardHeader2 from '../LeaderboardHeader2';
+
+storiesOf('LeaderboardHeader2', module)
+  .add('LeaderboardHeader2', () => (
+    <LeaderboardHeader2 />
+  ));
