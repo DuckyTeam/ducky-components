ace95c09534635013d0ac09d2ced1972270223c7
Johannes Andersen <johanes1@online.no>
2016-09-19 14:35:01 +0200

    Easier modification of barchart props

--- src/BarChart/stories.js
+++ src/BarChart/stories.js
@@ -1,43 +1,44 @@
 import React from 'react';
 import { storiesOf, action, linkTo } from '@kadira/storybook';
-import { withKnobs, object} from '@kadira/storybook-addon-knobs';
+import { withKnobs, number} from '@kadira/storybook-addon-knobs';
 import BarChart from './index';
 
-storiesOf('BarChart', module).addDecorator(withKnobs)
-  .add('State1', () => (
+const stories = storiesOf('BarChart', module);
+stories.addDecorator(withKnobs);
+stories.add('Initial', () => (
     <BarChart
-        data={object('Data',[
+        data={[
             {
-                value: 500,
+                value: number("Bar 1 value", 403),
                 color: "#8BC34A",
                 image: "http//:fsfkjsld.no/img.jpg",
                 label: "Strek1"
             },
             {
-                value: 1523,
+                value: number("Bar 2 value", 436),
                 color: "#CFD8FC",
                 image: "http//:fsfkjsld.no/img.jpg",
                 label: "Strek2"
             },
             {
-                value: 493,
+                value: number("Bar 3 value", 231),
                 color: "#CFD8FC",
                 image: "http//:fsfkjsld.no/img.jpg",
                 label: "Strek3"
             },
             {
-                value: 454,
+                value: number("Bar 4 value", 173),
                 color: "#CFD8FC",
                 image: "http//:fsfkjsld.no/img.jpg",
                 label: "Strek4"
             },
             {
-                value: 324,
+                value: number("Bar 5 value", 273),
                 color: "#FFC107",
                 image: "http//:fsfkjsld.no/img.jpg",
                 label: "Strek5"
             }
-        ])}
+        ].slice(0, number("Number of bars", 5))}
         graphID={2}
         height={"300px"}
     />
