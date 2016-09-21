6fd50cde72c6f75288c524164461513ca1dd3d3d
nathan-bird <nathan2484@hotmail.co.uk>
2016-09-21 16:10:29 +0100

    button circle

---
+++ src/ButtonCircle/stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {storiesOf} from '@kadira/storybook';
+import ButtonCircle from './index';
+
+storiesOf('ButtonCircle', module)
+  .add('ButtonCircle', () => (
+    <ButtonCircle>Vis mer</ButtonCircle>
+));
