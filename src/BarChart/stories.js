f1b388fdb7d64a309de908ae3c1fb166a2225044
sara3988 <sara3988@gmail.com>
2016-09-23 15:54:00 +0200

    commit Barchart - WIP

--- src/BarChart/stories.js
+++ src/BarChart/stories.js
@@ -40,6 +40,6 @@ stories.add('Initial', () => (
         height={"300px"}
         selected={number("Selected", 12)}
         member={number("Member of", 12)}
-        goals={[200, 400, 600, 800, 5000]}
+        goals={[200, 400, 600, 800]}
     />
   ));
