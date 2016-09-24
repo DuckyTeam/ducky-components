36b308d4d83dc7cdfadb04944e0844cd88eecfaf
Johannes Andersen <johanes1@online.no>
2016-09-24 23:49:18 +0200

    More refactoring

--- src/BarChart/chart.js
+++ src/BarChart/chart.js
@@ -20,34 +20,27 @@ d3Chart.create = (el, props, state) => {
 };
 
 d3Chart.update = (el, state, props) => {
-  const height = props.height - props.margin.top - props.margin.bottom;
-  const xAxisOffset = height + props.margin.top + 5;
+
+  //A lot of calculations, functions and definitions for the chart
+  state.height = props.height - props.margin.top - props.margin.bottom;
+  state.xAxisOffset = state.height + props.margin.top + 5;
+  state.barTextFontSize = 20;
+  state.textPadding = 6;
   state.speed = 250;
+  state.highestScore = d3.max(state.data, (data) => data.value);
   state.leaderId = state.data.filter((data) => data.value === d3.max(state.data, data => data.value))[0].id;
-  const barTextFontSize = 20;
-  const textPadding = 6;
-
-  const faceValues = state.goals.map((goal, index) => {
-    return index > 0 ? state.goals[index] - (goal - state.goals[index - 1]) / 2 : goal / 2;
-  }).slice(0, 4);
-
-  const getPath = (data) => {
-    if (data.id === state.member && data.id !== state.leaderId) {
-      return paths.leaf;
-    } else if (data.id === state.leaderId) {
-      return paths.crown;
+  state.nextGoal = (() => {
+    for (let index = 0; index < state.goals.length; index += 1) {
+      if (state.goals[index] > state.highestScore) {
+        return state.goals[index];
+      }
     }
-    return '';
-  };
-
-  const getIconClass = (data) => {
-    if (data.id === state.leaderId) {
-      return `${styles.leaderIcon} ${styles.iconPaths}`;
-    } else if (data.id === state.member) {
-      return `${styles.memberIcon} ${styles.iconPaths}`;
-    }
-    return `${styles.backgroundColorIcon} ${styles.iconPaths}`;
-  };
+    return state.goals[state.goals.length - 1];
+  })();
+  state.yAxisTickValues = state.goals.slice(0, d3.min([state.goals.indexOf(state.nextGoal), state.goals.length]) + 1);
+  state.yourScore = state.data.reduce((acc, dp) => {
+    return dp.id === state.member ? acc + dp.value : acc;
+  }, 0);
 
   const isSelectedByName = (label) => {
     let found = false;
@@ -60,17 +53,16 @@ d3Chart.update = (el, state, props) => {
     return found;
   };
 
-  const yourScore = state.data.reduce((acc, dp) => {
-    return dp.id === state.member ? acc + dp.value : acc;
-  }, 0);
-
+  //Define x and y scales
   const xScale = d3.scale.ordinal()
     .domain(state.data.map((data) => data.label))
     .rangeBands([0, props.width - props.margin.left - props.margin.right], 0.67);
 
-  const getBarWidth = () => d3.min([xScale.rangeBand(), 24]);
+  const yScale = d3.scale.linear()
+    .domain([0, d3.max([state.highestScore, state.nextGoal])])
+    .range([state.height - 4, 15 + state.barTextFontSize + state.textPadding * 2]);
 
-  const getTextX = (data) => getBarX(data) + getBarWidth(data) / 2;
+  state.barWidth = d3.min([xScale.rangeBand(), 24]);
 
   // Resize svg-canvas
   const svg = utils.selectSVG(props.id)
@@ -78,24 +70,7 @@ d3Chart.update = (el, state, props) => {
       .attr('height', props.height);
 
   // Move xaxis
-  utils.selectXAxisGroup(svg).attr("transform", `translate(${props.margin.left}, ${xAxisOffset})`);
-
-  const highestScore = d3.max(state.data, (data) => data.value);
-
-  const nextGoal = () => {
-    for (let index = 0; index < state.goals.length; index += 1) {
-      if (state.goals[index] > highestScore) {
-        return state.goals[index];
-      }
-    }
-    return state.goals[state.goals.length - 1];
-  };
-
-  const yAxisTickValues = state.goals.slice(0, d3.min([state.goals.indexOf(nextGoal()), state.goals.length]) + 1);
-  const yScale = d3.scale.linear()
-    .domain([0, d3.max([highestScore, nextGoal()])])
-    // Make sure all the stuff on top of graph actually fits inside canvas
-    .range([height - 4, 15 + barTextFontSize + textPadding * 2]);
+  utils.selectXAxisGroup(svg).attr("transform", `translate(${props.margin.left}, ${state.xAxisOffset})`);
 
   const xAxis = d3
       .svg
@@ -107,13 +82,9 @@ d3Chart.update = (el, state, props) => {
       .svg
       .axis()
       .scale(yScale)
-      .tickValues(state.goals.slice(0, d3.min([state.goals.indexOf(nextGoal()), state.goals.length]) + 1))
+      .tickValues(state.yAxisTickValues)
       .tickSize(-props.width, 0, 0)
       .orient("left");
-
-  const faces = svg.select(`.${styles.faceGroup}`).selectAll('svg').data(faceValues);
-  const yAxisTicks = svg.select(`.${styles.yAxisTickValuesGroup}`).selectAll('svg').data(yAxisTickValues);
-  const textLables = svg.select(`.${styles.textLables}`).selectAll('text').data(yAxisTickValues);
 
   // Transition in new axis
   utils.selectYAxisGroup(svg).transition().duration(state.speed).delay(state.speed).call(yAxis);
@@ -123,59 +94,50 @@ d3Chart.update = (el, state, props) => {
       return isSelectedByName(data) ? styles.selectedXTick : null;
     });
 
-  const enteredFaces = faces.enter();
+  drawLabels(svg, state, props, yScale);
+  drawBars(svg, state, props, xScale, yScale);
+  drawFaces(svg, state, props, yScale);
+};
 
-  enteredFaces.append('svg').attr({
-    viewBox: "0 0 768 768",
-    x: props.width - props.margin.right - 20,
-    y: data => yScale(data),
-    width: 20,
-    height: 20
-  }).append('path').attr({
-    d: (data, index) => paths.faces[index]
-  });
-  faces.exit().remove();
+const drawLabels = (svg, state, props, yScale) => {
 
-  faces.transition().delay(state.speed).duration(state.speed).attr({
-    x: props.width - props.margin.right - props.margin.left - 15,
-    y: yScale,
-  }).select('path')
-    .attr({
-      class: (data, index) => data <= yourScore ? styles[`faceActive${index}`] : styles.faceInactive
-    });
+  //TODO: Merge into one operation, sort out blinking
 
-  // For y axis tick lables
+  const yAxisTicks = svg.select(`.${styles.yAxisTickValuesGroup}`).selectAll('svg').data(state.yAxisTickValues);
+  const textLables = svg.select(`.${styles.textLables}`).selectAll('text').data(state.yAxisTickValues);
+
   const enteredTickValues = yAxisTicks.enter();
 
   enteredTickValues.append('svg').attr({
     viewBox: "0 0 768 768",
     width: 40,
     height: 12,
-    y: data => yScale(data) - 10,
+    y: data => yScale(data) - 15,
     x: -300
   }).append('path').attr({
     d: (data) => {
-      return (data <= yourScore) ? paths.check : paths.leaf;
+      return (data <= state.yourScore) ? paths.check : paths.leaf;
     },
     class: (data) => {
-      return (data <= yourScore) ? styles.progressedGoalsCheck : styles.toBeProgressedGoalsLeaf;
+      return (data <= state.yourScore) ? styles.progressedGoalsCheck : styles.toBeProgressedGoalsLeaf;
     }
   });
+
   yAxisTicks.exit().remove();
+
   yAxisTicks.transition().duration(state.speed).delay(state.speed).attr({
-    y: data => yScale(data) - 10,
+    y: data => yScale(data) - 15,
     x: -30
   }).select('path')
   .attr({
     d: (data) => {
-      return (data <= yourScore) ? paths.check : paths.leaf;
+      return (data <= state.yourScore) ? paths.check : paths.leaf;
     },
     class: (data) => {
-      return (data <= yourScore) ? styles.progressedGoalsCheck : styles.toBeProgressedGoalsLeaf;
+      return (data <= state.yourScore) ? styles.progressedGoalsCheck : styles.toBeProgressedGoalsLeaf;
     }
   });
 
-  // For y axis tick text
   const textLablesValues = textLables.enter();
 
   textLablesValues.append('text')
@@ -184,7 +146,7 @@ d3Chart.update = (el, state, props) => {
       x: -80,
       y: data => yScale(data),
       class: (data) => {
-        return (data <= yourScore) ? styles.progressedGoalsText : styles.toBeProgressedGoalsText;
+        return (data <= state.yourScore) ? styles.progressedGoalsText : styles.toBeProgressedGoalsText;
       },
       'font-size': '10px'
     });
@@ -194,25 +156,52 @@ d3Chart.update = (el, state, props) => {
     .text((data) => Number(data).toLocaleString())
     .attr({
       class: (data) => {
-        return (data <= yourScore) ? styles.progressedGoalsText : styles.toBeProgressedGoalsText;
+        return (data <= state.yourScore) ? styles.progressedGoalsText : styles.toBeProgressedGoalsText;
       },
       'font-size': '10px',
-      y: yScale,
+      y: d => yScale(d) - 5,
       x: 0
     });
+}
 
-  drawBars(svg, state, props, xScale, yScale);
-};
+const drawFaces = (svg, state, props, yScale) => {
+
+  //Variable and helper functions
+  const faceValues = state.goals.map((goal, index) => {
+    return index > 0 ? state.goals[index] - (goal - state.goals[index - 1]) / 2 : goal / 2;
+  }).slice(0, 4);
+
+  //Draw faces
+  const faces = svg.select(`.${styles.faceGroup}`).selectAll('svg').data(faceValues);
+  const enteredFaces = faces.enter();
+
+  enteredFaces.append('svg').attr({
+    viewBox: "0 0 768 768",
+    x: props.width - props.margin.right - 20,
+    y: data => yScale(data) - 10,
+    width: 20,
+    height: 20
+  }).append('path').attr({
+    d: (data, index) => paths.faces[index]
+  });
+
+  faces.exit().remove();
+
+  faces.transition().delay(state.speed).duration(state.speed).attr({
+    x: props.width - props.margin.right - props.margin.left - 15,
+    y: d => yScale(d) - 10,
+  }).select('path')
+    .attr({
+      class: (data, index) => state.goals[index] <= state.yourScore ? styles[`faceActive${index}`] : styles.faceInactive
+    });
+
+}
 
 const drawBars = (svg, state, props, xScale, yScale) => {
 
   //Bunch of messy helper functions and variables
-  const height = props.height - props.margin.top - props.margin.bottom;
-  const textPadding = 6;
-  const barTextFontSize = 20;
   const leaderId = state.data.filter((data) => data.value === d3.max(state.data, data => data.value))[0].id;
-  const getBarWidth = () => d3.min([xScale.rangeBand(), 24]);
-  const getBarX = (data) => xScale(data.label) + (xScale.rangeBand() - getBarWidth()) / 2
+  const getBarX = (data) => xScale(data.label) + (xScale.rangeBand() - state.barWidth) / 2
   const getIconClass = (data) => {
     if (data.id === leaderId) {
       return `${styles.leaderIcon} ${styles.iconPaths}`;
@@ -240,7 +229,8 @@ const drawBars = (svg, state, props, xScale, yScale) => {
     }
     return '10px';
   };
-  const getTextX = (data) => getBarX(data) + getBarWidth(data) / 2;
+
+  const getTextX = (data) => getBarX(data) + state.barWidth / 2;
 
   //Actually draw the rectangles
   const rects = svg.select(`.${styles.bars}`).selectAll("g").data(state.data, (data) => data.id).attr({class: getClasses});
@@ -251,17 +241,17 @@ const drawBars = (svg, state, props, xScale, yScale) => {
   entered.append("rect")
     .attr('class', styles.rectangle)
     .attr('rx', 2)
-    .attr('x', (data) => xScale(data.label) + (xScale.rangeBand() - getBarWidth()) / 2)
-    .attr('y', height)
+    .attr('x', (data) => xScale(data.label) + (xScale.rangeBand() - state.barWidth / 2))
+    .attr('y', state.height)
     .attr('height', 0)
-    .attr('width', () => d3.min([xScale.rangeBand(), 24]));
+    .attr('width', state.barWidth);
 
   entered.append('svg').attr({
     viewBox: "0 0 768 768",
     width: xScale.rangeBand(),
     height: xScale.rangeBand(),
-    x: (data) => xScale(data.label) + (xScale.rangeBand() - getBarWidth()) / 2,
-    y: height,
+    x: (data) => xScale(data.label) + (xScale.rangeBand() - state.barWidth / 2),
+    y: state.height,
     opacity: 0
   }).append('path').attr({
     class: getIconClass,
@@ -273,7 +263,7 @@ const drawBars = (svg, state, props, xScale, yScale) => {
       class: styles.barText,
       text: (data) => Number(data.value).toLocaleString(),
       x: (data) => xScale(data.label) + 0.5*xScale.rangeBand(),
-      y: height,
+      y: state.height,
       width: xScale.rangeBand(),
       height: xScale.rangeBand(),
       'font-size': getFontSize
@@ -281,7 +271,7 @@ const drawBars = (svg, state, props, xScale, yScale) => {
 
   // Remove unnessescary rectangles
   rects.exit().transition().duration(state.speed)
-    .attr('y', height)
+    .attr('y', state.height)
     .attr('height', 0)
     .remove();
 
@@ -289,12 +279,12 @@ const drawBars = (svg, state, props, xScale, yScale) => {
   const transX = rects.transition().delay(state.speed).duration(state.speed);
 
   transX.select("rect")
-    .attr('x', (data) => xScale(data.label) + (xScale.rangeBand() - getBarWidth()) / 2)
-    .attr('width', () => d3.min([xScale.rangeBand(), 24]))
+    .attr('x', getBarX)
+    .attr('width', state.barWidth)
     .attr('fill', (data) => data.color);
 
   transX.select("svg")
-    .attr({x: (data) => xScale(data.label) + (xScale.rangeBand() - getBarWidth()) / 2});
+    .attr('x', getBarX);
 
   transX.select("text")
     .text((data) => Number(data.value).toLocaleString())
@@ -308,10 +298,10 @@ const drawBars = (svg, state, props, xScale, yScale) => {
 
   transY.select("rect")
     .attr('y', (data) => yScale(data.value))
-    .attr('height', (data) => height - yScale(data.value));
+    .attr('height', (data) => state.height - yScale(data.value));
 
   transY.select("svg").attr({
-    y: (data) => yScale(data.value) - getBarWidth(data) - barTextFontSize,
+    y: (data) => yScale(data.value) - state.barWidth - state.barTextFontSize,
     width: () => d3.min([xScale.rangeBand(), 24]),
     height: () => d3.min([xScale.rangeBand(), 24]),
     opacity: 1
@@ -326,7 +316,7 @@ const drawBars = (svg, state, props, xScale, yScale) => {
     .text((data) => Number(data.value).toLocaleString())
     .attr({
       'font-size': getFontSize,
-      y: (data) => yScale(data.value) - textPadding
+      y: (data) => yScale(data.value) - state.textPadding
     });
 }
 
