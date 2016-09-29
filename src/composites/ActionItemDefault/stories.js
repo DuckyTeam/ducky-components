31095aa3de85124257b5cae59c852df71cc82c0e
nathan-bird <nathan2484@hotmail.co.uk>
2016-09-29 11:53:39 +0100

    ActionItemSelectComposit

--- src/composites/ActionItemDefault/stories.js
+++ src/composites/ActionItemDefault/stories.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {storiesOf, linkTo} from '@kadira/storybook';
+import {storiesOf} from '@kadira/storybook';
 import ActionItemDefault from './index';
 
-storiesOf('ActionItemDefault', module)
+storiesOf('ActionItem', module)
   .add('ActionItemDefault', () => (
     <div style={{width: 250}}>
       <ActionItemDefault
