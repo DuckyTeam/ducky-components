0c3028a5fbde03c3c1e380f4aae964a87e15dfa8
nathan-bird <nathan2484@hotmail.co.uk>
2016-10-07 13:32:38 +0100

    refactor privacy

--- src/SettingsPrivacyGeneral/index.js
+++ src/SettingsPrivacyGeneral/index.js
@@ -1,38 +1,22 @@
-import Wrapper from '../Wrapper';
-import Typography from '../Typography';
 import React from 'react';
-import classNames from 'classnames';
-import styles from './styles.css';
+import SettingsItem from '../SettingsItem';
 const PropTypes = React.PropTypes;
 
 function SettingsPrivacyGeneral(props) {
-    return (
-        <Wrapper className={classNames(styles.wrapper, {[props.className]: props.className})}
-            onClick={props.onClick}
-            size={'standard'}
-        >
-            <div className={styles.composite}>
-                <Typography
-                    className={styles.typoTitle}
-                    type={'bodyTextNormal'}
-                >
-                    {'Instilling'}
-                </Typography>
-                <Typography
-                    className={styles.typoContent}
-                    type={'bodyTextStrong'}
-                >
-                    {props.privacySettings || 'normal'}
-                </Typography>
-            </div>
-        </Wrapper>
-    );
+  return (
+    <SettingsItem
+      inactive
+      label={'Innstilling'}
+      onClick={props.onClick}
+      value={props.privacySettings ? props.privacySettings : 'normal'}
+    />
+  );
 }
 
 SettingsPrivacyGeneral.propTypes = {
-    className: PropTypes.string,
-    onClick: PropTypes.func,
-    privacySettings: PropTypes.string
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+  privacySettings: PropTypes.string
 };
 
 export default SettingsPrivacyGeneral;
