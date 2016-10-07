6b0b79fd9718458feacbdd66a955c5088033b649
nathan-bird <nathan2484@hotmail.co.uk>
2016-10-07 12:34:02 +0100

    refactor email

--- src/SettingsItemEmail/index.js
+++ src/SettingsItemEmail/index.js
@@ -1,42 +1,22 @@
-import Wrapper from '../Wrapper';
-import Typography from '../Typography';
+import SettingsItem from '../SettingsItem';
 import React from 'react';
-import classNames from 'classnames';
-import styles from './styles.css';
 const PropTypes = React.PropTypes;
 
 function SettingsItemEmail(props) {
-    return (
-        <Wrapper className={classNames(styles.wrapper, {
-            [styles.inactiveWrapper]: props.inactive,
-            [props.className]: props.className
-        })}
-            onClick={props.inactive ? props.onClick : ''}
-            size={'standard'}
-        >
-            <div className={styles.composite}>
-                <Typography
-                    className={props.inactive ? styles.typoTitleInactive : styles.typoTitle}
-                    type={'bodyTextNormal'}
-                >
-                    {'Epost'}
-                </Typography>
-                <Typography
-                    className={props.inactive ? styles.typoContentInactive : styles.typoContent}
-                    type={'bodyTextStrong'}
-                >
-                    {props.email}
-                </Typography>
-            </div>
-        </Wrapper>
-    );
+  return (
+    <SettingsItem
+      inactive
+      label={'Epost'}
+      onClick={props.onClick}
+      value={props.email}
+    />
+  );
 }
 
 SettingsItemEmail.propTypes = {
-    className: PropTypes.string,
-    email: PropTypes.string,
-    inactive: PropTypes.bool,
-    onClick: PropTypes.func
+  className: PropTypes.string,
+  email: PropTypes.string,
+  onClick: PropTypes.func
 };
 
 export default SettingsItemEmail;
