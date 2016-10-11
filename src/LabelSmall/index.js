84f0be17f6d5b9a1deb089f02a05ef830d30123a
Tommy Østgaard <tommy.ostgaard@gmail.com>
2016-10-11 17:11:53 +0200

    Refactored LabelSmall (issue 1710)

--- src/LabelSmall/index.js
+++ src/LabelSmall/index.js
@@ -6,30 +6,29 @@ import classNames from 'classnames';
 const PropTypes = React.PropTypes;
 
 function LabelSmall(props) {
-    return (
-        <span className={classNames(styles.wrapper, {[props.className]: props.className})}>
-            <Icon
-                className={classNames(styles.pad, {
-                    [styles.pointsIcon]: props.icon === 'icon-brightness_high',
-                    [styles.co2Icon]: props.icon === 'icon-leaf',
-                    [props.className]: props.className
-                })}
-                icon={props.icon}
-                onClick={props.onClick}
-                size={'micro'}
-            />
-            <Typography type={props.typographyType}>{props.content}</Typography>
-        </span>
-
-    );
+  return (
+    <span className={classNames(styles.wrapper, {[props.className]: props.className})}>
+      <Icon
+        className={classNames(styles.pad, {
+          [styles.pointsIcon]: props.icon === 'icon-brightness_high',
+          [styles.co2Icon]: props.icon === 'icon-leaf',
+          [props.className]: props.className
+        })}
+        icon={props.icon}
+        onClick={props.onClick}
+        size={'micro'}
+      />
+      <Typography type={props.typographyType || 'caption2Normal'}>{props.content}</Typography>
+    </span>
+  );
 }
 
 LabelSmall.displayName = 'LabelSmall';
 LabelSmall.propTypes = {
-    className: PropTypes.string,
-    content: PropTypes.node,
-    icon: PropTypes.string,
-    onClick: React.PropTypes.func,
-    typographyType: PropTypes.string
+  className: PropTypes.string,
+  content: PropTypes.node,
+  icon: PropTypes.string,
+  onClick: React.PropTypes.func,
+  typographyType: PropTypes.string
 };
 export default LabelSmall;
