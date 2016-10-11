246b3228ef6fcef9b7edcf54c86ff9a78a4de78f
nathan-bird <nathan2484@hotmail.co.uk>
2016-10-11 19:35:51 +0100

    linting

--- src/Typography/index.js
+++ src/Typography/index.js
@@ -3,40 +3,40 @@ import classNames from 'classnames';
 import styles from './styles.css';
 
 function Typography(props) {
-    return (
-        <span
-            className={classNames(styles.wrapper, {
-                [styles[props.type]]: props.type,
-                [props.className]: props.className
-            })}
-            onClick={props.onClick}
-        >
-          {props.children}
-        </span>
-    );
+  return (
+    <span
+      className={classNames(styles.wrapper, {
+        [styles[props.type]]: props.type,
+        [props.className]: props.className
+      })}
+      onClick={props.onClick}
+      >
+      {props.children}
+    </span>
+  );
 }
 
 Typography.propTypes = {
-    children: React.PropTypes.node,
-    className: React.PropTypes.string,
-    onClick: React.PropTypes.func,
-    type: React.PropTypes.oneOf([
-        'caption1Normal',
-        'caption1Strong',
-        'caption2Normal',
-        'caption2Strong',
-        'bodyTextNormal',
-        'bodyTextStrong',
-        'bodyTextTitle',
-        'ingressNormal',
-        'ingressStrong',
-        'ingressTitle',
-        'header1',
-        'display1',
-        'display2',
-        'display3',
-        'button'
-    ])
+  children: React.PropTypes.node,
+  className: React.PropTypes.string,
+  onClick: React.PropTypes.func,
+  type: React.PropTypes.oneOf([
+    'caption1Normal',
+    'caption1Strong',
+    'caption2Normal',
+    'caption2Strong',
+    'bodyTextNormal',
+    'bodyTextStrong',
+    'bodyTextTitle',
+    'ingressNormal',
+    'ingressStrong',
+    'ingressTitle',
+    'header1',
+    'display1',
+    'display2',
+    'display3',
+    'button'
+  ])
 };
 
 export default Typography;
--- src/Wrapper/index.js
+++ src/Wrapper/index.js
@@ -4,27 +4,27 @@ import styles from './styles.css';
 const PropTypes = React.PropTypes;
 
 function Wrapper(props) {
-    return (
-        <div
-            className={classNames(styles.wrapper, {
-                [styles[props.size]]: props.size,
-                [props.className]: props.className
-            })}
-            onClick={props.onClick}
-            style={props.style}
-        >
-            {props.children}
-        </div>
-    );
+  return (
+    <div
+      className={classNames(styles.wrapper, {
+        [styles[props.size]]: props.size,
+        [props.className]: props.className
+      })}
+      onClick={props.onClick}
+      style={props.style}
+      >
+        {props.children}
+    </div>
+  );
 }
 
 Wrapper.propTypes = {
-    children: PropTypes.node,
-    className: PropTypes.string,
-    onClick: PropTypes.func,
-    size: PropTypes.oneOf(['standard', 'side-bottom', 'side', 'narrow', 'slim',
-        'short', 'midget', 'squat', 'slender']),
-    style: PropTypes.object
+  children: PropTypes.node,
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+  size: PropTypes.oneOf(['standard', 'side-bottom', 'side', 'narrow', 'slim',
+    'short', 'midget', 'squat', 'slender']),
+  style: PropTypes.object
 };
 
 export default Wrapper;
