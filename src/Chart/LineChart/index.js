cfd5b04821b5f62f7d2ed4b070635441e32e0188
Johannes Andersen <johanes1@online.no>
2016-10-12 15:35:05 +0200

    Updated prop types of charts

--- src/Chart/LineChart/index.js
+++ src/Chart/LineChart/index.js
@@ -12,15 +12,15 @@ LineChart.PropTypes = {
   goals: React.PropTypes.arrayOf(React.PropTypes.number),
   graphID: React.PropTypes.number.isRequired,
   height: React.PropTypes.string.isRequired,
-  memberOf: React.PropTypes.number,
-  selectedId: React.PropTypes.number,
+  memberOf: React.PropTypes.any,
+  selectedId: React.PropTypes.any,
   onClick: React.PropTypes.func,
   isMobile: React.PropTypes.bool,
   startDate: React.PropTypes.string,
   endDate: React.PropTypes.string,
   data: React.PropTypes.arrayOf(React.PropTypes.shape({
     label: React.PropTypes.string,
-    id: React.PropTypes.number,
+    id: React.PropTypes.any,
     data: React.PropTypes.arrayOf(React.PropTypes.shape({
       date: React.PropTypes.string,
       value: React.PropTypes.number
