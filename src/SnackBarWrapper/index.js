9269bd9e376ad028015b8d835baa86792f31cc13
Svein Arne <sveinroed@gmail.com>
2016-10-19 11:37:11 +0200

    updated ShowActivitySubHeader with state for icons

--- src/SnackBarWrapper/index.js
+++ src/SnackBarWrapper/index.js
@@ -4,30 +4,28 @@ import styles from './styles.css';
 const PropTypes = React.PropTypes;
 
 class SnackBarWrapper extends React.Component {
-    render() {
-        if (!this.props.show) {
-            return null;
-        }
-
-        return (
-            <div className={classNames(styles.snackbarWrapper, {
-                [this.props.className]: this.props.className
-            })}
-            >
-                <div
-                    className={styles[(this.props.show ? 'showbottom' : 'bottom')]}
-                >
-                    {this.props.children}
-                </div>
-            </div>
-        );
+  render() {
+    if (!this.props.show) {
+      return null;
     }
+
+    return (
+      <div className={classNames(styles.snackbarWrapper, {
+        [this.props.className]: this.props.className})}>
+        <div
+          className={styles[(this.props.show ? 'showbottom' : 'bottom')]}
+          >
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
 }
 
 SnackBarWrapper.propTypes = {
-    children: PropTypes.node,
-    className: PropTypes.string,
-    show: PropTypes.bool
+  children: PropTypes.node,
+  className: PropTypes.string,
+  show: PropTypes.bool
 };
 
 export default SnackBarWrapper;
