91b2f6e8ba1851f2a9bb21a7d316a665c9a45cb3
franarave <franarave@gmail.com>
2016-10-20 11:35:39 +0200

    CreateGoalPanel1
    fixing component's problems
    change in ActionSelectComposit styles.css
    change in CreateGoalActionItem styles.css

--- src/CreateGoalPanel1/index.js
+++ src/CreateGoalPanel1/index.js
@@ -1,84 +1,79 @@
 import React from 'react';
-import classNames from 'classnames';
 import SectionHeaderGeneral from '../SectionHeaderGeneral';
 import GoalMenuItem from '../GoalMenuItem';
-import Spacer  from '../Spacer';
+import Spacer from '../Spacer';
 import styles from './styles.css';
 import SectionFooterNew from '../SectionFooterNew';
+const PropTypes = React.PropTypes;
 
+function CreateGoalPanel1(props) {
+  return (
+    <div className={styles.outerWrapper}>
+      <SectionHeaderGeneral
+        children={props.children}
+        rightIcon={"icon-close"}
+        title={props.title}
+        />
+      <Spacer
+        size={"standard"}
+        />
+      <GoalMenuItem
+        inactive={props.inactive}
+        onClick={props.onClick}
+        type={"co2"}
+        />
+      <Spacer
+        className={styles.hr2}
+        hr={"true"}
+        size={"hr2"}
+        />
+      <GoalMenuItem
+        inactive={props.inactive}
+        onClick={props.onClick}
+        type={'points'}
+        />
+      <Spacer
+        className={styles.hr2}
+        hr={"true"}
+        size={"hr2"}
+        />
+      <GoalMenuItem
+        inactive={props.inactive}
+        onClick={props.onClick}
+        type={'activity'}
+        />
+      <Spacer
+        className={styles.hr2}
+        hr={"true"}
+        size={"hr2"}
+        />
+      <GoalMenuItem
+        inactive={props.inactive}
+        onClick={props.onClick}
+        type={'habit'}
 
-function CreateGoalPanel1(props){
-
-      return (
-
-
-        <div className={styles.outerWrapper}>
-              <SectionHeaderGeneral
-                      onClick={props.onCancel}
-                      rightIcon={"icon-close"}
-                      title={props.title}
-                      children={props.children}
-                />
-
-                <Spacer
-                  size={"standard"}
-                />
-
-               <GoalMenuItem
-                inactive= {props.inactive}
-                onClick= {props.onClick}
-                type= {"co2"}
-                />
-                <Spacer  className={styles.hr2}
-                        size={"hr2"}
-                        hr={"true"}
-                />
-
-                <GoalMenuItem
-                inactive= {props.inactive}
-                onClick= {props.onClick}
-                type= {'points'}
-                />
-                <Spacer  className={styles.hr2}
-                        size={"hr2"}
-                        hr={"true"}
-                        padding-right={"16px"}
-                        padding-leftt={"68px"}
-                />
-                <GoalMenuItem
-                    inactive= {props.inactive}
-                    onClick= {props.onClick}
-                    type= {'habit'}
-                  />
-
-                  <Spacer className={styles.hr2}
-                          size={"hr2"}
-                          hr={"true"}
-
-                  />
-                  <GoalMenuItem
-                      inactive= {props.inactive}
-                      onClick= {props.onClick}
-                      type= {'activity'}
-                    />
-                    <Spacer
-                    size={'double'}
-                    hr={"true"}
-                    />
-               <SectionFooterNew type={props.type}/>
-          </div>
-
-)
-
+        />
+      <Spacer
+        hr={"true"}
+        size={'double'}
+        />
+      <SectionFooterNew
+        className={styles.footer}
+        text={props.text}
+        type= {'SEF005'}
+        />
+    </div>
+  );
 }
 
 CreateGoalPanel1.propTypes = {
-    className: React.PropTypes.string,
-    children: React.PropTypes.string,
-    inactive: React.PropTypes.bool,
-    onClick: React.PropTypes.func,
-    type: React.PropTypes.string,
-    size:React.PropTypes.string
+  children: PropTypes.string,
+  className: PropTypes.string,
+  inactive: PropTypes.bool,
+  onClick: PropTypes.func,
+  rightIcon: PropTypes.string,
+  text: PropTypes.string,
+  title: PropTypes.string,
+  type: PropTypes.string
 };
-
 export default CreateGoalPanel1;
--- src/CreateGoalPanel1/stories.js
+++ src/CreateGoalPanel1/stories.js
@@ -1,17 +1,14 @@
-import {bool, stories, oneOf, func, string, number, className} from '../duckyStories';
+import {bool, stories, func, string, className} from '../duckyStories';
 import CreateGoalPanel1 from './index';
-
 
 stories(module, CreateGoalPanel1, [
   'https://github.com/DuckyTeam/ducky-web/issues/1458'
 ], {
-
   className: className(),
   children: string('Bygg en vane eller fullfør prestasjoner iløpet av en fastsatt periode.'),
   title: string('Sett nytt personlig mål'),
   onClick: func(),
-  iconClicked: bool(),
-  inactive:bool(),
+  inactive: bool(),
   onCancel: func(),
-  type: string("AVBRYT")
-})
+  text: string('AVBRYT')
+});
