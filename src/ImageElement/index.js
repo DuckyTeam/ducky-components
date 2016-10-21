babe9689b754197c70fac81191a6005377d8a04b
nathan-bird <nathan2484@hotmail.co.uk>
2016-10-21 13:54:42 +0200

    ImageElement props

--- src/ImageElement/index.js
+++ src/ImageElement/index.js
@@ -2,31 +2,32 @@ import React from 'react';
 import {PropTypes} from 'react';
 
 function ImageElement(props) {
-    const style = {
-        backgroundImage: `url(${props.image})`,
-        backgroundSize: props.backgroundSize,
-        backgroundPosition: 'center center',
-        backgroundRepeat: 'no-repeat',
-        width: props.width,
-        height: props.height
-    };
+  const style = {
+    backgroundImage: `url(${props.image})`,
+    backgroundSize: props.backgroundSize,
+    backgroundPosition: 'center center',
+    backgroundRepeat: 'no-repeat',
+    width: props.width,
+    height: props.height
+  };
 
-    return (
-        <div className={props.className}>
-            <div
-                onClick={props.onClick}
-                style={style}
-            />
-        </div>
-    );
+  return (
+    <div className={props.className}>
+      <div
+        onClick={props.onClick}
+        style={style}
+        />
+    </div>
+  );
 }
 
 ImageElement.propTypes = {
-    className: PropTypes.string,
-    height: PropTypes.string.isRequired,
-    image: PropTypes.string,
-    onClick: PropTypes.func,
-    width: PropTypes.string.isRequired
+  backgroundSize: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  height: PropTypes.string.isRequired,
+  image: PropTypes.string,
+  onClick: PropTypes.func,
+  width: PropTypes.string.isRequired
 };
 
 export default ImageElement;
