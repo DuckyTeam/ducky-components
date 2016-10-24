af1db3606d8418c7c94aced473ccd6939124d8c4
nathan-bird <nathan2484@hotmail.co.uk>
2016-10-24 15:56:44 +0200

    switch cases

--- src/SettingsItemNotifications/index.js
+++ src/SettingsItemNotifications/index.js
@@ -7,60 +7,78 @@ import Wrapper from '../Wrapper';
 import styles from './styles.css';
 const PropTypes = React.PropTypes;
 
-function SettingsItemNotifications(props) {
-  return (
-    <SettingsItem
-      expanded={props.expanded}
-      handleCloseClick={props.onCloseClick}
-      handleSaveButtonClicked={props.onSaveButtonClick}
-      inactive={props.inactive}
-      label={'Nivå'}
-      onClick={props.onClick}
-      value={'tmp'}
-      >
-      <Wrapper className={styles.wrapper}
-        size={'standard'}
+class SettingsItemNotifications extends React.Component {
+  getNotificationValue(notificationValue) {
+    switch (notificationValue) {
+    case 0:
+      return 'Ikke forstyrr';
+    case 1:
+      return 'Reservert';
+    case 2:
+      return 'Moderat';
+    case 3:
+      return 'Nyskjerrig';
+    case 4:
+      return 'Alltid oppdatert';
+    default:
+      return null;
+    }
+  }
+  render() {
+    return (
+      <SettingsItem
+        expanded={this.props.expanded}
+        handleCloseClick={this.props.onCloseClick}
+        handleSaveButtonClicked={this.props.onSaveButtonClick}
+        inactive={this.props.inactive}
+        label={'Nivå'}
+        onClick={this.props.onClick}
+        value={this.getNotificationValue(this.props.notificationValue)}
         >
-        <ToggleButtonWithLabel
-          checked={props.checkedSocialWarnings}
-          label={'Sosiale varsler'}
-          />
-        <Typography
-          className={styles.notificationText}
-          type="caption2Normal"
+        <Wrapper className={styles.wrapper}
+          size={'standard'}
           >
-          {'For eksempel når noen liker eller kommenterer dine aktiviteter, eller begynner å følge deg.'}
-        </Typography>
-        <Spacer size={'large'} />
-        <ToggleButtonWithLabel
-          checked={props.checkedChallenges}
-          label={"Utfordringer"}
-          />
-        <Typography
-          className={styles.notificationText}
-          type="caption2Normal"
-          >
-          Oppdateringer om fremdrift på utfordringer du deltar i, og når nye utfordringer er tilgjengelige.
-        </Typography>
-        <Spacer size={'large'} />
-        <ToggleButtonWithLabel
-          checked={props.checkedPersonalGoals}
-          label={"Personlige mål"}
-          />
-        <Typography
-          className={styles.notificationText}
-          type="caption2Normal"
-          >
-          Oppdateringer om fremdrift på dine personlige mål.
-        </Typography>
-        <Spacer size={'large'} />
-        <ToggleButtonWithLabel
-          checked={props.checkedNewsletter}
-          label={'Motta nyhetsbrev'}
-          />
-      </Wrapper>
-    </SettingsItem>
-  );
+          <ToggleButtonWithLabel
+            checked={this.props.checkedSocialWarnings}
+            label={'Sosiale varsler'}
+            />
+          <Typography
+            className={styles.notificationText}
+            type="caption2Normal"
+            >
+            {'For eksempel når noen liker eller kommenterer dine aktiviteter, eller begynner å følge deg.'}
+          </Typography>
+          <Spacer size={'large'} />
+          <ToggleButtonWithLabel
+            checked={this.props.checkedChallenges}
+            label={"Utfordringer"}
+            />
+          <Typography
+            className={styles.notificationText}
+            type="caption2Normal"
+            >
+            {'Oppdateringer om fremdrift på utfordringer du deltar i, og når nye utfordringer er tilgjengelige.'}
+          </Typography>
+          <Spacer size={'large'} />
+          <ToggleButtonWithLabel
+            checked={this.props.checkedPersonalGoals}
+            label={"Personlige mål"}
+            />
+          <Typography
+            className={styles.notificationText}
+            type="caption2Normal"
+            >
+            {'Oppdateringer om fremdrift på dine personlige mål.'}
+          </Typography>
+          <Spacer size={'large'} />
+          <ToggleButtonWithLabel
+            checked={this.props.checkedNewsletter}
+            label={'Motta nyhetsbrev'}
+            />
+        </Wrapper>
+      </SettingsItem>
+    );
+  }
 }
 
 SettingsItemNotifications.propTypes = {
@@ -70,6 +88,7 @@ SettingsItemNotifications.propTypes = {
   checkedSocialWarnings: PropTypes.bool,
   expanded: PropTypes.bool,
   inactive: PropTypes.bool,
+  notificationValue: PropTypes.number,
   onClick: PropTypes.func,
   onCloseClick: PropTypes.func,
   onSaveButtonClick: PropTypes.func
--- src/SettingsItemNotifications/stories.js
+++ src/SettingsItemNotifications/stories.js
@@ -1,16 +1,17 @@
-import {stories, func, bool} from '../duckyStories';
+import {stories, func, bool, oneOf} from '../duckyStories';
 import SettingsItemNotifications from './index';
 
 stories(module, SettingsItemNotifications, [
   'https://github.com/DuckyTeam/ducky-web/issues/1587'
 ], {
-  checkedSocialWarnings:bool(),
-  checkedChallenges:bool(),
-  checkedPersonalGoals:bool(),
-  checkedNewsletter:bool(),
+  checkedSocialWarnings: bool(),
+  checkedChallenges: bool(),
+  checkedPersonalGoals: bool(),
+  checkedNewsletter: bool(),
   expanded: bool(),
   inactive: bool(),
   onClick: func(),
   onCloseClick: func(),
-  onSaveButtonClick: func()
+  onSaveButtonClick: func(),
+  notificationValue: oneOf(0, 1, 2, 3, 4)
 });
