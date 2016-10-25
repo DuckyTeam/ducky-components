40bde18a986f701ba289e836c1e480f2dedf8d32
Wayne bamford <bannerintheuk@gmail.com>
2016-10-25 10:22:49 +0100

    refactored SectionHeaderActivityModal to function

--- src/SectionHeaderActivityModal/index.js
+++ src/SectionHeaderActivityModal/index.js
@@ -4,49 +4,47 @@ import ButtonIcon from '../ButtonIcon';
 import classNames from 'classnames';
 import styles from './styles.css';
 
-class SectionHeaderActivityModal extends React.Component {
-    render() {
-        return (
-            <div className={classNames(styles.wrapper, {
-                [this.props.className]: this.props.className
-            })}
+function SectionHeaderActivityModal(props) {
+  return (
+    <div className={classNames(styles.wrapper, {
+      [props.className]: props.className
+    })}>
+      {props.favorite
+        ? <div className={styles.buttonIcon}>
+          <ButtonIcon
+            icon={'icon-star-off'}
+            onClick={props.handleButtonOnClick}
             >
-                {this.props.favorite
-                    ? <div className={styles.buttonIcon}>
-                        <ButtonIcon
-                            icon={'icon-star-off'}
-                            onClick={this.handleButtonOnClick}
-                        >
-                            {"Legg til favoritter"}
-                        </ButtonIcon>
-                    </div>
-                    : <div className={styles.buttonIcon}>
-                        <ButtonIcon
-                            icon={'icon-star_border'}
-                            onClick={this.handleButtonOnClick}
-                        >
-                            {"Fjern som favoritt"}
-                        </ButtonIcon>
-                    </div>
-                }
+            {"Legg til favoritter"}
+          </ButtonIcon>
+        </div>
+        : <div className={styles.buttonIcon}>
+          <ButtonIcon
+            icon={'icon-star_border'}
+            onClick={props.handleButtonOnClick}
+            >
+            {"Fjern som favoritt"}
+          </ButtonIcon>
+        </div>
+      }
 
-                <div className={styles.iconWrapper}>
-                    <IconAvaWrapper
-                        icon={'icon-close'}
-                        onClick={this.props.onIconClose}
-                        size={"standard"}
-                    />
-                </div>
-            </div>
-        );
-    }
+      <div className={styles.iconWrapper}>
+        <IconAvaWrapper
+          icon={'icon-close'}
+          onClick={props.onIconClose}
+          size={"standard"}
+          />
+      </div>
+    </div>
+  );
 }
 
 SectionHeaderActivityModal.propTypes = {
-    className: React.PropTypes.string,
-    favorite: React.PropTypes.bool,
-    onClick: React.PropTypes.func,
-    onIconClose: React.PropTypes.func
+  className: React.PropTypes.string,
+  favorite: React.PropTypes.bool,
+  handleButtonOnClick: React.PropTypes.func,
+  onClick: React.PropTypes.func,
+  onIconClose: React.PropTypes.func
 };
 
 export default SectionHeaderActivityModal;
