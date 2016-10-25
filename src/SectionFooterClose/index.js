98a88abc8c55cd7804796845caab5e360ac7bc4a
Wayne bamford <bannerintheuk@gmail.com>
2016-10-25 10:11:29 +0100

    refactored SectionFooterClose to function

--- src/SectionFooterClose/index.js
+++ src/SectionFooterClose/index.js
@@ -3,28 +3,25 @@ import Button from './../Button';
 import styles from './styles.css';
 import classNames from 'classnames';
 
-class SectionFooterClose extends React.Component {
-    render() {
-        return (
-            <div className={classNames(styles.wrapper, {
-                [this.props.className]: this.props.className}
-                )}
-            >
-                <Button
-                    className={styles.button}
-                    onClick={this.props.onClick}
-                >
-                    {"LUKK"}
-                </Button>
-            </div>
-        );
-    }
+function SectionFooterClose(props) {
+  return (
+    <div className={classNames(styles.wrapper, {
+      [props.className]: props.className}
+    )}>
+      <Button
+        className={styles.button}
+        onClick={props.onClick}
+        >
+        {"LUKK"}
+      </Button>
+    </div>
+  );
 }
 
 SectionFooterClose.displayName = "Section Footer Close";
 SectionFooterClose.propTypes = {
-    className: React.PropTypes.string,
-    onClick: React.PropTypes.func
+  className: React.PropTypes.string,
+  onClick: React.PropTypes.func
 };
 
 export default SectionFooterClose;
