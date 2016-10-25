30dd0bb256e0a94b7659481247eb0a3988985ef0
nathan-bird <nathan2484@hotmail.co.uk>
2016-10-25 11:00:25 +0200

    linting, icon background

--- src/SettingsSectionOne/stories.js
+++ src/SettingsSectionOne/stories.js
@@ -5,10 +5,10 @@ stories(module, SettingsSectionOne, [
   'https://github.com/DuckyTeam/ducky-web/issues/1573'
 ], {
   backgroundImgUrl: string('https://www.ducky.no/images/landing-image-1.jpg'),
-  changeCoverImage: func(),
-  changeProfileImage: func(),
   description: string('Description'),
-  editMode: func(),
   name: string('Per Thomas Normannsen'),
+  onCoverImageChange: func(),
+  onEditModeClick: func(),
+  onProfileImageChange: func(),
   profileImgUrl: string('https://cloud.githubusercontent.com/assets/13749577/13396610/ea8fe0e8-def5-11e5-8320-c0dd6fafe5bf.png')
 });
