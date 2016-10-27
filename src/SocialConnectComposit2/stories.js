88c1ea774d1754231693dc5c23bf1e85aebbc72b
Svein Arne <sveinroed@gmail.com>
2016-10-27 14:34:40 +0200

    Created SocialConnectComposit2
    some refactor

---
+++ src/SocialConnectComposit2/stories.js
@@ -0,0 +1,12 @@
+import {stories, bool, string, func, oneOf, className} from '../duckyStories';
+import SocialConnectComposit2 from './index';
+
+stories(module, SocialConnectComposit2, [
+  'https://github.com/DuckyTeam/ducky-web/issues/1335'
+], {
+  className: className(),
+  connectedFacebook: bool(false),
+  connectedGoogle: bool(false),
+  connectedTwitter: bool(true),
+  onClick: func(),
+});
