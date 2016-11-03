e0ba53603579d2bd371bffb44676e18549e49d08
Svein Arne <sveinroed@gmail.com>
2016-11-03 16:16:02 +0100

    Created CalculatorGeneralItems

---
+++ src/CalculatorGeneralItem/stories.js
@@ -0,0 +1,14 @@
+import {stories, string, bool, func} from '../duckyStories';
+import CalculatorGeneralItem from './index';
+
+stories(module, CalculatorGeneralItem, [
+  'https://github.com/DuckyTeam/ducky-web/issues/1953'
+], {
+  children: string(),
+  className: string(),
+  expanded: bool(),
+  inactive: bool(),
+  label: string("Hvor mye spiser du?"),
+  onClick: func(),
+  value: string("Gjennomsnitlig")
+});
