79f2e389956662cb13d95ece578b6237b918f65c
nathan-bird <nathan2484@hotmail.co.uk>
2016-11-03 14:13:21 +0100

    linting

--- src/SwitchToggleButton/index.js
+++ src/SwitchToggleButton/index.js
@@ -5,28 +5,28 @@ const PropTypes = React.PropTypes;
 
 function SwitchToggleButton(props) {
   return (
-        <label className={classNames(styles.groove, {
-          [styles.darkgroove]: props.theme === 'dark'}, {
-            [props.className]: props.className
-          })}>
-          <input
-            checked={props.checked}
-            defaultChecked={props.defaultChecked}
-            type="checkbox"
-            onChange={props.onChange}
-            />
-          <div className={classNames(styles.slider, {
-            [styles.darkslider]: props.theme === 'dark'}, {
-              [props.className]: props.className
-            })} />
-        </label>
-    );
+    <label className={classNames(styles.groove, {
+      [styles.darkgroove]: props.theme === 'dark'}, {
+        [props.className]: props.className
+      })}>
+      <input
+        checked={props.checked}
+        defaultChecked={props.defaultChecked}
+        onChange={props.onChange}
+        type="checkbox"
+        />
+      <div className={classNames(styles.slider, {
+        [styles.darkslider]: props.theme === 'dark'}, {
+          [props.className]: props.className
+        })} />
+    </label>
+  );
 }
 
 SwitchToggleButton.propTypes = {
   checked: PropTypes.bool,
-  defaultChecked: PropTypes.bool,
   className: PropTypes.string,
+  defaultChecked: PropTypes.bool,
   onChange: PropTypes.func,
   theme: PropTypes.oneOf(['light', 'dark'])
 };
