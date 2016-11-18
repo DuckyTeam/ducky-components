7ee63f9ab158cd115e5c3a13663157777e2e9f35
Svein Arne <sveinroed@gmail.com>
2016-11-18 14:56:33 +0100

    Created CounterInoutGeneral

--- src/CounterInputGeneral/index.js
+++ src/CounterInputGeneral/index.js
@@ -1,24 +1,48 @@
 import React from 'react';
 import classNames from 'classnames';
+import ButtonPlusMinus from '../ButtonPlusMinus';
+import Typography from '../Typography';
 import styles from './styles.css';
 
 function CounterInputGeneral(props) {
   return (
-    ""
+    <div className={classNames(styles.wrapper)}>
+      <Typography
+        className={classNames(styles.counterText, [props.className]: props.className)}
+        type={'bodyTextNormal'}
+        >
+        {props.label}
+      </Typography>
+      <Typography
+        className={classNames([props.className]: props.className)}
+        type={'bodyTextNormal'}
+        >
+        {props.resultNumber}
+      </Typography>
+      <ButtonPlusMinus
+        className={classNames(styles.minus, [props.className]: props.className)}
+        inactive={props.inactiveNegative}
+        onClick={props.onClick}
+        operator="remove"
+        />
+      <ButtonPlusMinus
+        className={classNames(styles.plus, [props.className]: props.className)}
+        inactive={props.inactivePositive}
+        onClick={props.onClick}
+        operator="plus"
+        />
+    </div>
   );
 }
 
 CounterInputGeneral.propTypes = {
-  children: React.PropTypes.node,
   className: React.PropTypes.string,
-  expanded: React.PropTypes.bool,
-  inactive: React.PropTypes.bool,
+  inactiveNegative: React.PropTypes.bool,
+  inactivePositive: React.PropTypes.bool,
   label: React.PropTypes.string,
   onClick: React.PropTypes.func,
-  onCloseClick: React.PropTypes.func,
-  onSaveButtonClicked: React.PropTypes.func,
-  saveButtonText: React.PropTypes.string,
-  value: React.PropTypes.string
+  operator: React.PropTypes.oneOf(['plus', 'remove']),
+  resultNumber: React.PropTypes.string
 };
 
 export default CounterInputGeneral;
