da4b25ecfe90479acb4b98a45f58e2a1bf3af97d
franarave <franarave@gmail.com>
2016-11-28 12:01:15 +0100

    Update ProB01
    https://github.com/DuckyTeam/ducky-web/issues/2100,
    Minor detail, update default progress bar background color

--- src/ProgressBar/stories.js
+++ src/ProgressBar/stories.js
@@ -1,12 +1,13 @@
-import {stories, className, string, func, number, oneOf} from '../duckyStories';
+import {stories, className, string, func, number, json, oneOf} from '../duckyStories';
 import ProgressBar from './index';
 
 stories(module, ProgressBar, [
   'https://github.com/DuckyTeam/ducky-web/issues/900',
-  'https://github.com/DuckyTeam/ducky-web/issues/1931'
+  'https://github.com/DuckyTeam/ducky-web/issues/1931',
+  'https://github.com/DuckyTeam/ducky-web/issues/2100'
 ], {
   className: className(),
-  color: string('red'),
+  style: json({color: '#607d8b'}),
   onClick: func(),
   percent: number(25),
   size: oneOf('standard', 'wide')
