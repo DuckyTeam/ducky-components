7a2d70e533bbceafd6e08be9da2679e830f3a937
Tommy Østgaard <tommy.ostgaard@gmail.com>
2016-11-30 16:42:56 +0100

    Commit

---
+++ src/SectionHeaderSubsection/index.js
@@ -0,0 +1,44 @@
+import IconAvaWrapper from '../IconAvaWrapper';
+import React from 'react';
+import LabelTitle from '../LabelTitle';
+import Wrapper from '../Wrapper';
+import classNames from 'classnames';
+import styles from './styles.css';
+
+function SectionHeaderSubsection(props) {
+  return (
+    <div className={classNames(styles.wrapper, {
+      [props.className]: props.className
+    })}>
+      <Wrapper
+        className={styles.iconTextWrapper}
+        size="short"
+        >
+        <LabelTitle
+          icon={props.leftIcon}
+          size="regular"
+          text={props.title}
+          />
+      </Wrapper>
+      {props.rightIcon
+        ? <IconAvaWrapper
+          icon={props.rightIcon}
+          onClick={props.onRightIconClick}
+          size={"standard"}
+          />
+        : null
+      }
+    </div>
+  );
+}
+
+SectionHeaderSubsection.propTypes = {
+  className: React.PropTypes.string,
+  leftIcon: React.PropTypes.string,
+  onClick: React.PropTypes.func,
+  onRightIconClick: React.PropTypes.func,
+  rightIcon: React.PropTypes.string,
+  title: React.PropTypes.string
+};
+
+export default SectionHeaderSubsection;
---
+++ src/SectionHeaderSubsection/stories.js
@@ -0,0 +1,13 @@
+import {stories, className, string, func} from '../duckyStories';
+import SectionHeaderSubsection from './index';
+
+stories(module, SectionHeaderSubsection, [
+  'https://github.com/DuckyTeam/ducky-web/issues/2136'
+], {
+  className: className(),
+  onClick: func(),
+  leftIcon: string('icon-star'),
+  rightIcon: string('icon-leaf'),
+  onRightIconClick: func(),
+  title: string('Applikasjoner')
+});
