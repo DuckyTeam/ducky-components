a9f5d3fb1ee30e7143bb0cbb29d22f126a3fda22
bannerintheuk <bannerintheuk@gmail.com>
2016-12-06 20:44:17 +0000

    Added PlaceholderElement1

---
+++ src/PlaceholderElement1/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Icon from '../Icon';
+import Wrapper from '../Wrapper';
+import Spacer from '../Spacer';
+import Typography from '../Typography';
+import classNames from 'classnames';
+import styles from './styles.css';
+
+const PropTypes = React.PropTypes;
+
+function PlaceholderElement1(props) {
+  return (
+    <div className={classNames(styles.outerWrapper)}>
+      <Spacer size={'double'} />
+      <Icon
+        className={styles.icon}
+        icon={props.icon}
+        size={'display2'}
+        />
+      <Wrapper
+        className={styles.textWrapper}
+        type={'standard'}
+        >
+        <Typography
+          className={styles.text}
+          type={'bodyTextNormal'}
+          >{props.text}</Typography>
+      </Wrapper>
+    </div>
+  );
+}
+
+PlaceholderElement1.propTypes = {
+  icon: PropTypes.string,
+  text: PropTypes.string
+};
+
+export default PlaceholderElement1;
