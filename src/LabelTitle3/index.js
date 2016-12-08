b6b9d39293324d6d80ed3ef6b296b20e09054515
nathan-bird <nathan2484@hotmail.co.uk>
2016-12-08 18:39:32 +0000

    fix

---
+++ src/LabelTitle3/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {PropTypes} from 'react';
+import classNames from 'classnames';
+import styles from './styles.css';
+import Typography from '../Typography';
+import Icon from '../Icon';
+
+function LabelTitle3(props) {
+  return (
+    <span className={classNames(styles.wrapper, {[props.className]: props.className})}>
+      <Icon
+        className={styles.icon}
+        icon={props.icon}
+        size="mini"
+        style={props.style}
+        />
+      <Typography
+        className={styles.label}
+        type="caption2Normal"
+        >
+        {props.label}
+      </Typography>
+    </span>
+    );
+}
+
+LabelTitle3.propTypes = {
+  className: PropTypes.string,
+  icon: PropTypes.string,
+  label: PropTypes.string,
+  style: React.PropTypes.shape({})
+};
+
+export default LabelTitle3;
