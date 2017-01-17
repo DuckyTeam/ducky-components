a44f167dea63d741b7dc3a412843ba829432137a
Maria <franarave>
2017-01-17 15:50:28 +0100

    Iss2171

--- src/AccordionItem/index.js
+++ src/AccordionItem/index.js
@@ -1,32 +1,72 @@
+import IconAvaWrapper from '../IconAvaWrapper';
 import React from 'react';
+import Icon from '../Icon';
+import Wrapper from '../Wrapper';
+import Typography from '../Typography';
+import classNames from 'classnames';
 import styles from './styles.css';
-import SectionHeaderSubsection from '../SectionHeaderSubsection';
-const propTypes = 'React.PropTypes';
 
 function AccordionItem(props) {
   return (
     props.expanded
-    ? <SectionHeaderSubsection
-      className={styles.outerWrapperExpanded}
-      leftIcon={props.icon}
-      rightIcon={'icon-keyboard_arrow_up'}
-      title={props.title}
-      />
-  : <SectionHeaderSubsection
-    className={styles.outerWrapperExpanded}
-    leftIcon={props.icon}
-    rightIcon={'icon-keyboard_arrow_down'}
-    title={props.title}
-    />
+    ? <div className={classNames(styles.wrapper, {
+      [props.className]: props.className
+    })}
+      onClick={props.onClick}
+      >
+      <Wrapper
+        className={styles.iconTextWrapper}
+        size="short"
+        >
+        <Icon
+          className={styles.active}
+          icon={props.leftIcon}
+          size={'regular'}
+          />
+        <Typography
+          type="bodyTextNormal"
+          >
+          {props.title}
+        </Typography>
+      </Wrapper>
+      <IconAvaWrapper
+        icon={'icon-keyboard_arrow_down'}
+        size={"standard"}
+        />
+    </div>
+    : <div className={classNames(styles.wrapper, {
+      [props.className]: props.className
+    })}
+      onClick={props.onClick}>
+      <Wrapper
+        className={styles.iconTextWrapper}
+        size="short"
+        >
+        <Icon
+          className={styles.inactive}
+          icon={props.leftIcon}
+          size={'regular'}
+          />
+        <Typography
+          type="bodyTextNormal"
+          >
+          {props.title}
+        </Typography>
+      </Wrapper>
+      <IconAvaWrapper
+        icon={'icon-keyboard_arrow_up'}
+        size={"standard"}
+        />
+    </div>
   );
 }
 
 AccordionItem.propTypes = {
-  children: propTypes.node,
-  expanded: propTypes.bool,
-  icon: propTypes.string,
-  onClick: propTypes.func,
-  title: propTypes.title
+  className: React.PropTypes.string,
+  expanded: React.PropTypes.bool,
+  leftIcon: React.PropTypes.string,
+  onClick: React.PropTypes.func,
+  title: React.PropTypes.string
 };
 
 export default AccordionItem;
--- src/AccordionItem/stories.js
+++ src/AccordionItem/stories.js
@@ -1,11 +1,12 @@
-import {stories, string, bool, func} from '../duckyStories';
+import {stories, className, string, func, bool} from '../duckyStories';
 import AccordionItem from './index';
 
 stories(module, AccordionItem, [
   'https://github.com/DuckyTeam/ducky-web/issues/2171'
 ], {
-  children: string(),
-  className: string(),
+  className: className(),
+  onClick: func(),
   expanded: bool(),
-  title: string("Viernes de cervezas")
+  leftIcon: string('icon-star'),
+  title: string('AccordionItem')
 });
