e7b46bc3a83507e6d50a504628ed837c3947dd44
bannerintheuk <bannerintheuk@gmail.com>
2017-01-30 22:00:03 +0000

    latest

--- src/Chart/index.js
+++ src/Chart/index.js
@@ -1,59 +1,58 @@
 import React from 'react';
 
 class Chart extends React.Component {
-    constructor() {
-        super();
-        this.margin = {
-          top: 15,
-          bottom: 40,
-          left: 50,
-          right: 8
-        };
-        this.container = null;
-    }
-    componentDidMount() {
-        this.margin.left = this.props.milestones || this.props.goal ? 50 : 8;
-        this.props.chart.create(this.container, {
-            width: this.container.offsetWidth,
-            height: this.container.offsetHeight,
-            margin: this.margin,
-            id: this.props.graphID
-        }, this.getChartState());
-    }
+  constructor() {
+    super();
+    this.margin = {
+      top: 15,
+      bottom: 40,
+      left: 50,
+      right: 8
+    };
+    this.container = null;
+  }
+  componentDidMount() {
+    this.margin.left = this.props.milestones || this.props.goal ? 50 : 8;
+    this.props.chart.create(this.container, {
+      width: this.container.offsetWidth,
+      height: this.container.offsetHeight,
+      margin: this.margin,
+      id: this.props.graphID
+    }, this.getChartState());
+  }
 
-    componentDidUpdate() {
-        this.margin.left = this.props.milestones || this.props.goal ? 50 : 8;
-        this.props.chart.update(this.container, this.getChartState(), {
-            width: this.container.offsetWidth,
-            height: this.container.offsetHeight,
-            margin: this.margin,
-            id: this.props.graphID
-        }, this.props.formatting);
-    }
+  componentDidUpdate() {
+    this.margin.left = this.props.milestones || this.props.goal ? 50 : 8;
+    this.props.chart.update(this.container, this.getChartState(), {
+      width: this.container.offsetWidth,
+      height: this.container.offsetHeight,
+      margin: this.margin,
+      id: this.props.graphID
+    }, this.props.formatting);
+  }
 
-    componentWillUnmount() {
-      this.props.chart.destroy(this.props.graphID);
-    }
+  componentWillUnmount() {
+    this.props.chart.destroy(this.props.graphID);
+  }
 
-    handleRef = (element) => {
-        this.container = element;
-    }
+  handleRef = (element) => {
+    this.container = element;
+  }
 
-    getChartState() {
-        return Object.assign({}, this.props);
-    }
+  getChartState() {
+    return Object.assign({}, this.props);
+  }
 
-    render() {
-        return (
+  render() {
+    return (
             <div
-                className="Chart"
-                id={`chartDiv${this.props.graphID}`}
-                ref={this.handleRef}
-                style={{height: this.props.height}}
-            >
-            </div>
+              className="Chart"
+              id={`chartDiv${this.props.graphID}`}
+              ref={this.handleRef}
+              style={{height: this.props.height}}
+              />
         );
-    }
+  }
 }
 
 export default Chart;
