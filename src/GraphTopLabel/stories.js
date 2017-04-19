18351d7d4a498125a0703ec282e2f1cc456e8da6
nathan-bird <nathan2484@hotmail.co.uk>
2017-04-19 17:07:49 +0100

    remove component

--- src/GraphTopLabel/stories.js
+++
@@ -1,9 +0,0 @@
-import {stories, string, oneOf} from '../duckyStories';
-import GraphTopLabel from './index';
-
-stories(module, GraphTopLabel, [
-  'https://github.com/DuckyTeam/ducky-web/issues/2976'
-], {
-  caption: string(''),
-  challengeType: oneOf('team', 'household', 'individual')
-});
